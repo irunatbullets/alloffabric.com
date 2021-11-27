32fbf2bff81ab8032a3aa0c1618b6bd2de10d2bf
irunatbullets <irunatbullets@users.noreply.github.com>
2021-11-27 17:10:04 +0000

    deploy: 00a9b64f104e0dd84b94d9f36ba3612534927626

--- _nuxt/static/1638032735/testing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Falloffabric.com\u002F_nuxt\u002Fstatic\u002F1638032735",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftesting",config:{_app:{basePath:"\u002Falloffabric.com\u002F",assetsPath:"\u002Falloffabric.com\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0d71291a"}}}}(null));
