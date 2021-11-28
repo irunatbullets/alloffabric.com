05184f599988fc5ebe7dea20e3af7725653f3b28
irunatbullets <murray.nuttall@gmail.com>
2021-11-28 09:19:11 +0100

    Moving images out of static - seems messy

--- plugins/helpers.js
+++ plugins/helpers.js
@@ -1,7 +1,11 @@
 const fileName = (string) => string.replace('/', '')
 const lowerCase = (string) => string.toLowerCase().trim()
+const imageSrc = (string) => require(`~/assets/uploads/${string}`)
+
+
 
 export default ({ app }, inject) => {
   inject('fileName', fileName)
   inject('lowerCase', lowerCase)
+  inject(`imageSrc`, imageSrc)
 }
