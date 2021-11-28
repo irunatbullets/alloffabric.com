d61cf31d76cc68f984e8cf1619f7a28c3a9023cd
irunatbullets <murray.nuttall@gmail.com>
2021-11-28 20:37:25 +0100

    Made a compact modifier for container

--- components/Container.vue
+++ components/Container.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container" :class="{'container--compact' : compact}">
     <div class="container__inner">
       <slot />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    compact: Boolean
+  }
+}
+</script>
 
 <style lang="postcss">
   .container {
@@ -29,5 +37,10 @@
     @media (--laptop) {
       padding: 144px 60px 144px 160px;
     }
+  }
+
+  .container--compact .container__inner {
+    padding-top: 0;
+    padding-bottom: 0;
   }
 </style>
--- components/blocks/Products.vue
+++ components/blocks/Products.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
-
-    {{block.title}}
-
-    <div v-for="(product, index) in products" :key="index">
-      <product
-        :type="product.type"
-        :title="product.title"
-        :image="product.image"
-        :description="product.description"
-        :categories="product.categories"
-        :mod-count="product.mod_count"
-        :button="product.button"
-        :color="product.color"
-      />
-    </div>
-
+    <container compact>
+      <h1>{{block.title}}</h1>
+    </container>
+    <product
+      v-for="(product, index) in products"
+      :key="index"
+      :type="product.type"
+      :title="product.title"
+      :image="product.image"
+      :description="product.description"
+      :categories="product.categories"
+      :mod-count="product.mod_count"
+      :button="product.button"
+      :color="product.color"
+    />
   </div>
 </template>
 
 <script>
+import Container from '../Container.vue'
 export default {
+  components: { Container },
   props: {
     props: {
       type: Object,
